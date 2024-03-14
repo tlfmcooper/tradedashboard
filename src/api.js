967aee9885841a3d148ecd211f1f8facb1eeb3c8
Ali Kone <tlfmcooper@gmail.com>
2024-03-13 22:44:11 -0400

    modified the server url

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.SERVER_URL || 'https://trade-1tdl7zam7-tlfmcoopers-projects.vercel.app/data';
+const API_URL = process.env.SERVER_URL || 'https://world-trade-murex.vercel.app/data';
 
 
 export const fetchData = async () => {
